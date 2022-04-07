ec87c7b7170125f25cdf1ffd451bb240199ccfb3
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2022-04-07 14:56:36 +0700

    Update catagory.js

--- assets/js/catagory.js
+++ assets/js/catagory.js
@@ -1,6 +1,6 @@
 
 function shuffer(){
-    fetch('https://www.ltp110.tk/shopee/assets/js/db.json')
+    fetch('./assets/js/db.json')
         .then(response => response.json())
         .then(list => {
             list = list.sort(() => Math.random() - 0.5)
